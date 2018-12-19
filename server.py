2b46d6b23fdb719f14939f4acf4654b30bbd8940
rowan <rowan@rowan.website>
2018-12-19 02:02:05 -0500

    Server accepts json files and sends them along dmx

--- server.py
+++ server.py
@@ -1,92 +1,28 @@
 import sys
-from flask import Flask, render_template
+from flask import abort, Flask, render_template, request, Response
 sys.path.insert(0, 'pyenttec/')
 import pyenttec
 
 app = Flask(__name__)
 dmx = pyenttec.select_port()
 
+# {
+#   "channels_list": [
+#     {"channel": 50, "value": 255},
+#     {"channel": 51, "value": 0},
+#     {"channel": 52, "value": 0}
+#   ]
+# }
 
-@app.route('/')
-def index():
-    return render_template('light.html')
+@app.route('/', methods=['POST'])
+def jsonHandler():
+    try:
+        contents = request.get_json()['channels_list']
+        for content in contents:
+            # print(content['channel'],': ',content['value'])
+            dmx.dmx_frame[content['channel']] = content['value']
 
-
-def make_color(color):
-    dmx.dmx_frame[144] = color['red']
-    dmx.dmx_frame[145] = color['green']
-    dmx.dmx_frame[146] = color['blue']
-
-    dmx.render()
-
-
-@app.route('/white')
-def white():
-    color = {'red': 255, 'green': 255, 'blue': 255}
-
-    make_color(color)
-
-    return render_template('light.html')
-
-
-@app.route('/red')
-def red():
-    color = {'red': 255, 'green': 0, 'blue': 0}
-
-    make_color(color)
-
-    return render_template('light.html')
-
-
-@app.route('/green')
-def green():
-    color = {'red': 0, 'green': 255, 'blue': 0}
-
-    make_color(color)
-
-    return render_template('light.html')
-
-
-@app.route('/blue')
-def blue():
-    color = {'red': 0, 'green': 0, 'blue': 255}
-
-    make_color(color)
-
-    return render_template('light.html')
-
-
-@app.route('/purple')
-def purple():
-    color = {'red': 255, 'green': 0, 'blue': 255}
-
-    make_color(color)
-
-    return render_template('light.html')
-
-
-@app.route('/teal')
-def teal():
-    color = {'red': 0, 'green': 255, 'blue': 255}
-
-    make_color(color)
-
-    return render_template('light.html')
-
-
-@app.route('/yellow')
-def yellow():
-    color = {'red': 255, 'green': 255, 'blue': 0}
-
-    make_color(color)
-
-    return render_template('light.html')
-
-
-@app.route('/off')
-def off():
-    color = {'red': 0, 'green': 0, 'blue': 0}
-
-    make_color(color)
-
-    return render_template('light.html')
+        dmx.render()
+        return Response(status=204, mimetype='application/json')
+    except TypeError:
+        abort(400)
