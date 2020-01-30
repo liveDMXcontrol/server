debe16e7fdb4c9769eafc3aaf26e55bc1bea76a9
rowan <rowan@rowan.website>
2020-01-29 22:16:33 -0500

    Add some print statements for debugging

--- server.py
+++ server.py
@@ -20,6 +20,7 @@ dmx = pyenttec.select_port()
 def jsonHandler():
     try:
         contents = request.get_json()['channels_list']
+        print(contents)
         for content in contents:
             # print(content['channel'],': ',content['value'])
             dmx.dmx_frame[content['channel']] = content['value']
@@ -27,4 +28,7 @@ def jsonHandler():
         dmx.render()
         return Response(status=204, mimetype='application/json')
     except TypeError:
+        print('TypeError')
         abort(400)
+    except:
+        print('other error')
